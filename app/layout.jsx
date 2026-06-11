904ff352d4bffb7478bc949df025a43d4af98aea
joaoc23dias <joao@gaiagroup.com.br>
2026-06-11 09:41:12 -0300

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -7,6 +7,17 @@ export default function RootLayout({ children }) {
     return (
           <html lang="pt-BR">
                 <body className="bg-white">{children}</body>body>
-          </html>html>
+          </html>html>export const metadata = {
+          title: 'Gaia Group | Consórcio Inteligente',
+          description: 'Método SPA - Transformando consórcio em alavancagem',
+    };
+
+    export default function RootLayout({ children }) {
+          return (
+                  <html lang="pt-BR">
+                        <body className="bg-white">{children}</body>body>
+                  </html>html>
+                );
+    }</html>
         );
 }</html>
